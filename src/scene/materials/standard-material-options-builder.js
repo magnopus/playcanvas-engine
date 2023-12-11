b8c3c9410d456a7ec5cb588c21dc7fbb8968cc6e
Adrian Meredith <adrian.meredith@magnopus.com>
2023-12-11 10:17:44 +0000

    [OW-1573] fix: apply skybox intensity to all materials

--- src/scene/materials/standard-material-options-builder.js
+++ src/scene/materials/standard-material-options-builder.js
@@ -342,7 +342,8 @@ class StandardMaterialOptionsBuilder {
         }
 
         // TODO: add a test for if non skybox cubemaps have rotation (when this is supported) - for now assume no non-skybox cubemap rotation
-        options.litOptions.skyboxIntensity = usingSceneEnv && (scene.skyboxIntensity !== 1 || scene.physicalUnits);
+        // magnopus patched, allow skybox intensity on all cubemaps
+        options.litOptions.skyboxIntensity = (scene.skyboxIntensity !== 1 || scene.physicalUnits);
 
         // magnopus patched, allow cubemap rotation on all cubemaps
         // options.litOptions.useCubeMapRotation = usingSceneEnv && scene._skyboxRotationShaderInclude;
