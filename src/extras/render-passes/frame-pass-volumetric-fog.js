43daab5b9a6480c4486cbfa3414902923a945bd3
Martin Valigursky <59932779+mvaligursky@users.noreply.github.com>
2026-08-13 11:20:35 +0200

    Declare scene depth map encoding instead of inferring from device caps (#9165)
    Co-authored-by: Martin Valigursky <mvaligursky@snapchat.com>
    (cherry picked from commit cef603fcadc4dc6af05f751a8ae9b55400f95c47)

--- src/extras/render-passes/frame-pass-volumetric-fog.js
+++ src/extras/render-passes/frame-pass-volumetric-fog.js
@@ -114,8 +114,8 @@ class RenderPassVolumetricFog extends RenderPassShaderQuad {
      */
     updateShaderVariant(shadows, pcf) {
 
-        const depthMapLinear = this.cameraComponent.shaderParams.sceneDepthMapLinear;
-        const key = `${shadows}-${pcf}-${depthMapLinear}`;
+        const depthKey = ShaderUtils.getScreenDepthChunkKey(this.cameraComponent.shaderParams);
+        const key = `${shadows}-${pcf}${depthKey}`;
         if (this._variantKey !== key) {
             this._variantKey = key;
 
@@ -240,10 +240,10 @@ class RenderPassVolumetricFogCombine extends RenderPassShaderQuad {
         this.fogTexture = fogTexture;
 
         const defines = new Map();
-        ShaderUtils.addScreenDepthChunkDefines(cameraComponent.shaderParams, defines);
+        const depthKey = ShaderUtils.addScreenDepthChunkDefines(cameraComponent.shaderParams, defines);
 
         this.shader = ShaderUtils.createShader(device, {
-            uniqueName: `VolumetricFogCombineShader-${cameraComponent.shaderParams.sceneDepthMapLinear}`,
+            uniqueName: `VolumetricFogCombineShader${depthKey}`,
             attributes: { aPosition: SEMANTIC_POSITION },
             vertexChunk: 'quadVS',
             fragmentChunk: 'volumetricFogCombinePS',
