6b265114c21d580361607756883822302b402d17
MAG-DanielMurdolo <daniel.murdolo@magnopus.com>
2026-08-24 11:45:16 +0100

    Add comments

--- src/framework/parsers/sog-bundle.js
+++ src/framework/parsers/sog-bundle.js
@@ -99,6 +99,7 @@ const inflate = async (compressed) => {
 };
 
 const downloadArrayBuffer = async (url, asset) => {
+    // Magnopus patched - added crossOrigin option
     const response = await (asset.file?.contents ?? fetch(url.load, asset.options?.crossOrigin === 'use-credentials' ? { credentials: 'include' } : undefined));
     if (!response) {
         throw new Error('Error loading resource');
