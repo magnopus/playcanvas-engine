2e9cf1870aa7f7c61b67f17cefd305b24fd3d500
James Edgeworth <james.edgeworth@magnopus.com>
2026-09-03 13:10:22 +0100

    - Added asset options to GSplat assets.

--- src/framework/parsers/gsplat-octree.js
+++ src/framework/parsers/gsplat-octree.js
@@ -44,13 +44,16 @@ class GSplatOctreeParser {
         const options = {
             retry: this.maxRetries > 0,
             maxRetries: this.maxRetries,
-            responseType: Http.ResponseType.JSON
+            responseType: Http.ResponseType.JSON,
+            // Magnopus patched - add withCredentials option
+            withCredentials: asset?.options?.crossOrigin === 'use-credentials'
         };
 
         http.get(url.load, options, (err, data) => {
             if (!err) {
                 // create a resource with the parsed data, passing the asset's file URL
-                const assetLoader = new GSplatAssetLoader(this.app.assets);
+                // magnopus patched - pass in the asset options.
+                const assetLoader = new GSplatAssetLoader(this.app.assets, asset?.options);
                 // magnopus patched
                 const resolveUrl = relativeUrl => this.app.resolveUrl(relativeUrl, {
                     asset,
